0d080ebbe2494576980eb5504eddba3b10951a9e
rish0303 <107429877+rish0303@users.noreply.github.com>
2023-05-18 23:43:27 -0700

    Simple ECS hpps added

---
+++ Alohaware/Source/Component.hpp
@@ -0,0 +1,14 @@
+/*!
+ * @author		Rishabh Anand
+ * @brief		Entitiy Definition
+!*/
+
+#pragma once
+
+#include <iostream>
+
+// A simple type alias
+using ComponentType = std::uint8_t;
+
+// Used to define the size of arrays later on
+const ComponentType MAX_COMPONENTS = 32;
---
+++ Alohaware/Source/Entity.hpp
@@ -0,0 +1,13 @@
+/*!
+ * @author		Rishabh Anand
+ * @brief		Entitiy Definition
+!*/
+
+#pragma once
+
+#include <limits>
+
+
+using Entity = long;
+
+constexpr Entity MaxEntities = std::numeric_limits<Entity>::max();
